d232f52eaa8d4a396e11a94ac6fcf89bc0f8b5c5
Roel Janssen <roel@roelserve.com>
2014-07-30 21:41:45 +0200

    Make use of the high-level functions to performs common tasks.

--- src/main.c
+++ src/main.c
@@ -26,6 +26,7 @@
 #include <time.h>
 #include <unistd.h>
 
+#include "high/command.h"
 #include "parsers/repository.h"
 #include "network/http.h"
 #include "database/installation.h"
@@ -150,50 +151,8 @@ main (int argc, char** argv)
       if (argc <= 2 || !strcmp (argv[2], "--help"))
 	show_usage ("enable <domain.tld>");
 
-      /* These variables a purely there for a clearer understanding
-       * of the code that follows. */
-      const char* domain = argv[2];
-
-      curl_global_init (CURL_GLOBAL_ALL);
-
-      /* Get the name and packages from the remote host. */
-      dt_http_response* response;
-      response = net_http_get ("http", domain, "moefel.repo", 80, NULL);
-
-      curl_global_cleanup();
-
-      /* Only when the status code is 200 we should process the entry. */
-      if (response->status != 200)
-	{
-	  puts ("Could not get repository information.");
-	  dt_http_response_free (response);
-	  return 1;
-	}
-
-      /* Parse the gathered information and store it in a 'dt_repository'. */
-      dt_repository* repo;
-      if (p_repository_from_buffer (&repo, response->body) == 0)
-	{
-	  printf ("Error while parsing packages from '%s'.\n", domain);
-	  return 0;
-	}
-
-      /* Clean up the memory allocated for the HTTP response. */
-      dt_http_response_free (response);
-
-      /* Set the domain name. */
-      repo->domain = calloc (1, strlen (domain) + 1);
-      repo->domain = strcpy (repo->domain, domain);
-
-      /* Set the current local time as 'created_at'. */
-      repo->created_at = m_current_timestamp();
-
-      /* Add the repository to the database. */
-      if (!db_repositories_add (DATABASE_NAME, repo))
-	printf ("Failed to add '%s'.\n", repo->name);
-
-      /* Clean up the dynamically allocated memory. */
-      dt_repository_free (repo);
+      if (h_command_enable (argv[2]))
+	printf ("'%s' has been added.\n", argv[2]);
     }
 
   /* OPTION: disable
@@ -217,51 +176,9 @@ main (int argc, char** argv)
    */
   else if (!strcmp (argv[1], "update"))
     {
-      curl_global_init (CURL_GLOBAL_ALL);
-      dt_list* repos = NULL;
-      if (!db_repositories_get_all (DATABASE_NAME, &repos))
-	{
-	  puts ("Couldn't get repositories from the database.");
-	  return 1;
-	}
-
-      dt_list* r = repos;
-      while (r != NULL)
-	{
-	  dt_repository* repo = r->data;
-	  r = r->next;
-
-	  /* Skip disabled repositories. */
-	  if (repo->is_enabled == 0) 
-	    {
-	      dt_repository_free (repo);
-	      continue;
-	    }
-	  printf ("Updating '%s'\n", repo->domain);
-
-	  /* Get the name and packages from the remote host. */
-	  dt_http_response* response;
-	  response = net_http_get ("http", repo->domain, "moefel.repo", 80, NULL);
-
-	  if (response->status == 200)
-	    {
-	      dt_repository* content;
-	      p_repository_from_buffer (&content, response->body);
-	      content->domain = calloc (1, strlen (repo->domain) + 1);
-	      content->domain = strcpy (content->domain, repo->domain);
-	      db_repositories_add (DATABASE_NAME, content);
-	      dt_repository_free (content);
-	    }
-
-	  /* Clean up and move on. */
-	  dt_http_response_free (response);
-	  dt_repository_free (repo);
-	}
-
-      dt_list_free (repos);
-      curl_global_cleanup();
+      if (h_command_update () > 0)
+	puts ("The package lists have been updated.");
     }
-
 
   /* OPTION: get
    * ----------------------------------------------------------------------
